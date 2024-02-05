cb7c28b0a2177408ec16dddd37f9cf638b01974f
Bhavya <eternallybhavya@gmail.com>
2024-02-06 00:02:14 +0530

    Upload

---
+++ 01-helloworld.py
@@ -0,0 +1,24 @@
+#PRINT STATEMENTS
+print("Hello World")
+print(7)
+print(5+6)
+print("This is a \bprint statement", 10)
+print("Using separator", 6, 7, sep="~", end="009")
+
+#PRINTING MULTIPLE LINES
+print(" GGS\nGGS2")
+print(""" GGS
+GGS2""")
+
+#COMMENTS
+#this is a single line comment
+"""
+this is a multiline comment (also works with single quote)
+"""
+
+#ESCAPE SEQUENCES
+# \n - New line
+# \" - Double Quote
+# \' - Single Quote
+# \t - Tab Space
+# \b

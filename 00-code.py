7625438b90ed37c27055275c9732f055b2c2a607
Bhavya <eternallybhavya@gmail.com>
2024-02-15 21:41:19 +0530

    .

---
+++ 00-code.py
@@ -0,0 +1,34 @@
+# # PRINT STATEMENTS
+# print("Hello World")
+# print(7)
+# print(5+6)
+# print("Hello World", 10)
+# print("Hello 'World'")
+# print("Using separator", 6, 7, sep="~", end="009")
+# print(" Statement1\nStatement2")
+# print(""" Statement1
+# Statement2""")
+
+
+# # ESCAPE SEQUENCES
+# \n - New line
+# \" - Double Quote
+# \' - Single Quote
+# \t - Tab Space
+
+
+# # FROZEN SETS
+# a = frozenset([1, 2, 3, 4, 5])
+# print(type(a))
+
+# # BITWISE OPERATION
+# a = 10
+# b = 4
+# print(a&b)
+
+x = 5
+print(type(not(5)))
+
+y="bhavya"
+z="vdfgsd"
+print(y&z)
